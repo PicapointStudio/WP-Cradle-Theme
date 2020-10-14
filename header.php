297ffebb0f68131e6711d8ac6b5247f2721fba68
Marius Bøe <marius@picapoint.no>
2020-10-14 15:10:02 +0200

    Small updates to template

--- header.php
+++ header.php
@@ -31,21 +31,6 @@
 	</head>
 			
 	<body <?php body_class(); ?>>
-				
-		<header class="header" id="site_header" role="banner">
-			<div class="container">
-				<div class="row">
-					<div class="column">
-						<a href="<?php echo home_url(); ?>" title="" class="site_logo">Logo</a>
-						<div id="toggle_mobile_menu" class="hamburger hamburger--collapse" aria-label="<?php _e('Toggle menu visibility', 'wpcradle'); ?>">
-							<div class="hamburger-box">
-							<div class="hamburger-inner"></div>
-							</div>
-						</div>
-					</div>
-					<div class="column">
-						<?php cradle_top_nav(); ?>
-					</div>
-				</div>
-			</div>
-		</header> <!-- end .header -->
+	<?php 
+	//Avaiable header templates: header-dynamic, header-sticky
+	get_template_part('parts', 'header-dynamic'); ?>
