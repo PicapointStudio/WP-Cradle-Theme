1cbaa0097594f59d878c1ea242119bed77698e5e
Martin Singstad <40832199+msingstad@users.noreply.github.com>
2021-10-11 20:25:02 +0200

    Update site_script.js

--- assets/scripts/js/site_script.js
+++ assets/scripts/js/site_script.js
@@ -1,20 +1,2 @@
 //Start your javascript journey here
 
-// Add smooth scroll to all anchor elements referencing an #id
-document.querySelectorAll('a[href^="#"]')
-    .forEach(trigger => {
-        trigger.onclick = function(e) {
-            e.preventDefault();
-            let hash = this.getAttribute('href');
-            let target = document.querySelector(hash);
-            let headerOffset = 100;
-            let elementPosition = target.offsetTop;
-            let offsetPosition = elementPosition - headerOffset;
-
-            window.scrollTo({
-                top: offsetPosition,
-                behavior: "smooth"
-            });
-        };
-    })
-;
